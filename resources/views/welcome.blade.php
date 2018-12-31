c0a6d020409bd7164e1dc37cb32ea9c58f11e0d3
xm11211 <980172892@qq.com>
2018-12-31 13:43:11 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <style>
+        html,body {
+            height: 100%;
+        }
+        body {
+            margin: 0;
+            padding: 0;
+            display: table;
+            font-family: 'Lato';
+            width: 100%;
+            font-weight: 100;
+        }
+        .container {
+            display: table-cell;
+            vertical-align: middle;
+            text-align: center;
+        }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+        .title {
+            font-size: 66px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">Hello Laravel! - by Summer</div>
+    </div>
+</div>
+</body>
+</html>
